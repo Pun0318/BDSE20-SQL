1c9a2f2b25f8d22d162dc8c9cbdee448ed1f0d01
Pun0318 <86967789+Pun0318@users.noreply.github.com>
2021-08-26 11:51:07 +0800

    Delete crew table.sql

--- crew table.sql
+++
@@ -1,9 +0,0 @@
-create table crew (
-crew_id int(20) ,
-movie_id  int(20),
-crew_name varchar(150) null,
-known_for_department varchar(150) null,
-department varchar(150) null,
-job varchar(150) null,
-FOREIGN KEY (movie_id) REFERENCES moviedata(movie_id)
-);
