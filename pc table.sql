13caa41167c583750519e8fdf4d4779d16279f55
Pun0318 <86967789+Pun0318@users.noreply.github.com>
2021-08-26 11:52:06 +0800

    Delete pc table.sql

--- pc table.sql
+++
@@ -1,8 +0,0 @@
-create table productioncompany (
-pc_id int(20) , 
-movie_id int(20),
-movie_title varchar(150) null,
-pc_name varchar(150) null,
-pc_country varchar(150) null,
-FOREIGN KEY (movie_id) REFERENCES moviedata(movie_id)
-);
